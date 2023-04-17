ad4b56ab27dd26ded572aebf9cfadcb4d265121f
Dorcas Laryea <naa1995ad@gmail.com>
2023-04-17 21:16:30 +0000

    initial setup for a basic todo app

---
+++ Data/Migrations/20230322235045_initialsetup.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDoApp.Data.Migrations
+{
+    public partial class initialsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Task",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    MainTask = table.Column<string>(type: "TEXT", nullable: false),
+                    SubTask = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Task", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Task");
+        }
+    }
+}
---
+++ Models/Task.cs
@@ -0,0 +1,13 @@
+namespace ToDoApp.Models;
+
+public class Task
+{
+    public int Id { get; set; }
+    public string MainTask { get; set; }
+    public string SubTask { get; set; }
+
+    public Task()
+    {
+            
+    }
+}
